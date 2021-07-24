6b71231244be96f171be80d6b3437cf636b7897e
repzlaw <ibitoyedavid@gmail.com>
2021-07-24 12:27:34 +0100

    adjusted messages :adhesive_bandage:

--- resources/views/inc/message.blade.php
+++ resources/views/inc/message.blade.php
@@ -1,26 +1,55 @@
 @if (count($errors) > 0)
     @foreach ($errors->all() as $error )
-        <div class="alert alert-danger">
-            {{$error}}
+    <div class="col-12 col-md-12">
+        <div class="row">
+            <div class="col-12 col-md-9"></div>
+            <div class="col-12 col-md-3">
+                <div class="alert alert-danger float-right">
+                    {{$error}}
+                </div>
+
+            </div>
         </div>
+    </div>
     @endforeach
 @endif
 
 @if (session('success'))
-    <div class="alert alert-success">
-        {{session('success')}}
+    <div class="col-12 col-md-12">
+        <div class="row">
+            <div class="col-12 col-md-9"></div>
+            <div class="col-12 col-md-3">
+                <div class="alert alert-success">
+                    {{session('success')}}
+                </div>
+            </div>
+        </div>
     </div>
 @endif
 
 @if (session('error'))
-    <div class="alert alert-danger">
-        {{session('error')}}
+    <div class="col-12 col-md-12">
+        <div class="row">
+            <div class="col-12 col-md-9"></div>
+            <div class="col-12 col-md-3">
+                <div class="alert alert-danger">
+                    {{session('error')}}
+                </div>
+            </div>
+        </div>
     </div>
 @endif
 
 
 @if (session('message'))
-    <div class="alert alert-info">
-        {{session('message')}}
+    <div class="col-12 col-md-12">
+        <div class="row">
+            <div class="col-12 col-md-9"></div>
+            <div class="col-12 col-md-3">
+                <div class="alert alert-info">
+                    {{session('message')}}
+                </div>
+            </div>
+        </div>
     </div>
 @endif
